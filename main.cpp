1d78a70e4bc149414afbb829d48892d4a2d6e53d
Pantro Huerta <pantro12@live.com.mx>
2021-11-29 10:46:50 -0600

    datos iniciales

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
