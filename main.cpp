cae1d9b9478ae9b35840d97442d94599ffbd37af
Pantro Huerta <pantro12@live.com.mx>
2021-12-01 18:06:05 -0600

    Se termino de configurar el hash

--- main.cpp
+++ main.cpp
@@ -11,16 +11,41 @@
 #include <cstring>
 
 #include "menu.h"
-#include "hash.h"
 
 using namespace std;
 
 int main(){
-
+    bool flag= true;
+    int funcion;
     Menu mainz;
 
-    //mainz.usarAVL();
-    mainz.crearGrafo();
+    do
+    {
+        cout << "Bienvenido a la biblioteca, seleccione que funcion desea llevar a cabo" << endl 
+             << "1. Encontrar camino a un pasillo en especifico." << endl << "2. Consultar datos por pasillo." << endl
+             << "3. Consultar libros por numero de hojas" << endl << "4. Correr pruebas automaticas" << endl 
+             << "Eleccion: ";
 
-    return 0;
+        cin >> funcion;
+
+        switch (funcion)
+        {
+        case 1:
+            mainz.usarGrafo();
+            break;
+        case 2:
+            mainz.usarHash();
+            break;
+        case 3:
+            mainz.usarAVL();
+            break;
+        case 4:
+            mainz.pruebasAutomaticas();
+            break;
+        
+        default:
+            cout << "Ingrese un valor valido" << endl;
+            break;
+        }
+    } while (flag);
 }
